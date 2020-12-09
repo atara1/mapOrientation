506bac8d16c28ff3eb93a8d57252e971fbb6d657
atara1 <atarabu49@gmail.com>
2020-12-09 16:48:39 +0200

    add a test for panel

--- e2e/src/informationPamel.e2e-spec.ts
+++ e2e/src/informationPamel.e2e-spec.ts
@@ -1,35 +1,12 @@
-import { TestBed, getTestBed } from '@angular/core/testing';
-import { MapService } from './../../src/app/service/map.service';
-import { AppPage } from './app.po';
-import { LocationData } from 'src/assets/locationData';
-import { Injector } from '@angular/core';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+import { AppPage } from './app.po';
 
 describe('information panel test', () => {
     let page: AppPage;
-    let mapService: MapService;
-    let injector: Injector;
-    let httpMock: HttpTestingController; // HttpClientTestingModule;
 
     beforeEach(() => {
         page = new AppPage();
-
-        // TestBed.configureTestingModule({
-        //     imports: [HttpClientTestingModule],
-        //     providers: [MapService]
-        // });
-
-        // injector = getTestBed();
-        // httpMock = injector.get(HttpTestingController);
-        // mapService = injector.get(MapService);
-
     });
-
-    // afterEach(() => {
-    //     httpMock.verify();
-    // });
-
 
 
     it('should be a Add annotation button', () => {
@@ -53,18 +30,5 @@ describe('information panel test', () => {
         console.log(input.all.length);
         expect(input.all.length == 1).toBeTruthy();
     });
-
-
-    // it('should present location on map', () => {
-    //     const inputSearch = page.setInputText('tel aviv');
-    //     mapService.search(inputSearch.toString())
-    //         .then(data => {
-    //             let lngLatData: LocationData = data;
-    //             if (lngLatData) {
-    //                 mapService.buildMap(lngLatData.center[0], lngLatData.center[1])
-    //             }
-    //         });
-    // });
-
 
 });
---
+++ e2e/src/map.e2e-spec.ts
@@ -0,0 +1,32 @@
+// import { MapComponent } from './../../src/app/map/map.component';
+
+// import { ComponentFixture, inject, TestBed, getTestBed, async } from '@angular/core/testing';
+// import { MapService } from './../../src/app/service/map.service';
+// import { AppPage } from './app.po';
+// import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+// import {
+//     BrowserDynamicTestingModule,
+//     platformBrowserDynamicTesting
+// } from '@angular/platform-browser-dynamic/testing';
+// import { HttpClientModule } from '@angular/common/http';
+// import { Type } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+
+
+// describe('map test', () => {
+//     let page: AppPage;
+//     let service: MapService;
+//     let httpMock: HttpTestingController;
+
+//     beforeEach(async () => {
+//         TestBed.configureTestingModule({
+//             imports: [HttpClientTestingModule],
+//             providers: [MapService]
+//         });
+//         //  service = TestBed.get(MapService);
+//     });
+
+
+
+// });
