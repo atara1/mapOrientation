92e0d6e0960d8010af3fb83edbd7c48fd7337a04
atara1 <atarabu49@gmail.com>
2020-12-16 11:17:21 +0200

    change let to const

--- src/app/service/map.service.ts
+++ src/app/service/map.service.ts
@@ -13,30 +13,30 @@ import { throwError } from 'rxjs';
 })
 export class MapService {
   map: mapboxgl.Map;
-  zoom = 12
+  zoom = 12;
   constructor(private httpClient: HttpClient) {
 
   }
 
-  public buildMap(Longitude: number, Latitude: number) {
+  public buildMap(Longitude: number, Latitude: number): void {
     this.map = new mapboxgl.Map({
       accessToken: environment.mapbox.accessToken,
       container: 'map',
       style: environment.mapbox.style,
       zoom: this.zoom,
       center: [Longitude, Latitude]
-    })
+    });
     this.map.addControl(new mapboxgl.NavigationControl());
     this.createMark(Longitude, Latitude);
   }
 
   public search(placeName: string): Promise<LocationData> {
-    let res: LocationData = new LocationData();
+    const res: LocationData = new LocationData();
     const searchTerm = placeName.toLowerCase();
     if (searchTerm && searchTerm.length > 0) {
       return this.searchLocation(searchTerm)
         .then((featurs) => {
-          let findLocation = featurs?.find(ele => {
+          const findLocation = featurs?.find(ele => {
             return ele.place_name.toLowerCase().includes(placeName.toLowerCase());
           });
           if (!!findLocation) {
@@ -51,7 +51,7 @@ export class MapService {
     }
   }
 
-  private createMark(lng: number, lat: number) {
+  private createMark(lng: number, lat: number): void {
     const marker = new mapboxgl.Marker({
       draggable: true
     }).setLngLat([lng, lat])
@@ -69,12 +69,12 @@ export class MapService {
   }
 
   private searchLocation(query: string): Promise<features[]> {
-    let url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
-    return this.httpClient.get(url + query + `.json?access_token=${environment.mapbox.accessToken}`)
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
+    return this.httpClient.get( `${url}${query}.json?access_token=${environment.mapbox.accessToken}`)
       .pipe(map((res: mapboxOutput) => {
         return res.features;
       }),
-        catchError(this.handelError)).toPromise()
+        catchError(this.handelError)).toPromise();
   }
 
 }
