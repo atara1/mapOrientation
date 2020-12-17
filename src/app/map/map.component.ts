071fa8636739d3e6bd2514abaf91703a0447b0cc
atara1 <atarabu49@gmail.com>
2020-12-17 18:06:49 +0200

    unsubscribe

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -16,7 +16,7 @@ export class MapComponent implements OnInit, OnDestroy, AfterViewInit {
   searchLocationData$: Observable<LocationInfo>;
   isLocationNotFound = false;
   centerMarker: [number, number] = [0, 0];
-  private subscription: Subscription;
+  private subscriptions: Subscription[] = [];
   mapbox: Map; /* this is the mapbox of angular - ngx-mapbox-gl */
 
   constructor(private store: Store<AppState>) {
@@ -28,22 +28,22 @@ export class MapComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngOnInit(): void {
-    this.subscription = this.searchLocationData$
+    this.subscriptions.push(this.searchLocationData$
       .subscribe(data => {
         if (data && data.center) {
           this.mapbox.setCenter(data.center);
           this.centerMarker = data.center;
         }
-      });
-    this.searchLocationData$.pipe(skip(1)).subscribe(data => {
+      }));
+    this.subscriptions.push(this.searchLocationData$.pipe(skip(1)).subscribe(data => {
       this.isLocationNotFound = !(data && data.center);
-    });
+    }));
 
   }
 
   @HostListener('window:unload', ['$event'])
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 
 }
