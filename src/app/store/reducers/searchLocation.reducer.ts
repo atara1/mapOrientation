691f9ea8dc592c313c1e593a2d64938fbb6b3072
atara1 <atarabu49@gmail.com>
2020-12-17 17:46:08 +0200

    fix searchLocation state

--- src/app/store/reducers/searchLocation.reducer.ts
+++ src/app/store/reducers/searchLocation.reducer.ts
@@ -2,11 +2,14 @@ import { Action, createReducer, on } from '@ngrx/store';
 import { LocationInfo } from '../../store/modles/locationInfo.modle';
 import * as searchLocation from '../actions/searchLocation.action';
 
+
+
 const defaultState: LocationInfo = { center: null, text: '' };
+
 
 const reducer = createReducer(
   defaultState,
-  on(searchLocation.Update, (newData: LocationInfo) => {
+  on(searchLocation.Update, (state: LocationInfo, newData: LocationInfo) => {
           return newData;
   })
 );
